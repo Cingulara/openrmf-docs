0b0f866c3bca668269c8deb4996de7a481d69303
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-04-15 20:29:49 -0400

    renaming and updating for openRMF startup scripts

---
+++ dev-start.cmd
@@ -0,0 +1,17 @@
+REM pull down the most recent :latest tags
+docker pull cingulara/openstig-web
+docker pull cingulara/openstig-api-read
+docker pull cingulara/openstig-api-save
+docker pull cingulara/openstig-api-upload
+docker pull cingulara/openstig-api-scoring
+docker pull cingulara/openstig-msg-score
+docker pull cingulara/openstig-templatedb
+docker pull cingulara/openstig-scoredb
+docker pull cingulara/openstig-checklistdb
+docker pull cingulara/openstig-api-template
+docker pull cingulara/openstig-api-compliance
+docker pull cingulara/openstig-api-controls
+docker pull nats
+
+REM Now run the latest development openRMF stack
+docker-compose -f dev-stack.yml up -d
