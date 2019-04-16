70f8ccf6d9246de3b6233f1d7932822ffd2c3c69
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-04-16 18:56:57 -0400

    update for openRMF

--- dev-start.sh
+++ dev-start.sh
@@ -1,16 +1,16 @@
 # pull down the most recent :latest tags
 docker pull cingulara/openrmf-web
-docker pull cingulara/openstig-api-read
-docker pull cingulara/openstig-api-save
-docker pull cingulara/openstig-api-upload
-docker pull cingulara/openstig-api-scoring
-docker pull cingulara/openstig-msg-score
-docker pull cingulara/openstig-templatedb
-docker pull cingulara/openstig-scoredb
-docker pull cingulara/openstig-checklistdb
-docker pull cingulara/openstig-api-template
-docker pull cingulara/openstig-api-compliance
-docker pull cingulara/openstig-api-controls
+docker pull cingulara/openrmf-api-read
+docker pull cingulara/openrmf-api-save
+docker pull cingulara/openrmf-api-upload
+docker pull cingulara/openrmf-api-scoring
+docker pull cingulara/openrmf-msg-score
+docker pull cingulara/openrmf-templatedb
+docker pull cingulara/openrmf-scoredb
+docker pull cingulara/openrmf-checklistdb
+docker pull cingulara/openrmf-api-template
+docker pull cingulara/openrmf-api-compliance
+docker pull cingulara/openrmf-api-controls
 docker pull nats
 
 # Now run the latest development openRMF stack
