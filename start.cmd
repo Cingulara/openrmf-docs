d55bcaf1f0c5f11d4e5d26b1e29e12526ec2f13b
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-09-18 18:23:18 -0400

    updating to use 0.8 images

--- start.cmd
+++ start.cmd
@@ -1,16 +1,19 @@
 REM pull down the most recent :latest tags
-docker pull cingulara/openrmf-web:0.7
-docker pull cingulara/openrmf-api-read:0.7
-docker pull cingulara/openrmf-api-save:0.7
-docker pull cingulara/openrmf-api-upload:0.7
-docker pull cingulara/openrmf-api-scoring:0.7
-docker pull cingulara/openrmf-msg-score:0.7
-docker pull cingulara/openrmf-templatedb:0.7
-docker pull cingulara/openrmf-scoredb:0.7
-docker pull cingulara/openrmf-checklistdb:0.7
-docker pull cingulara/openrmf-api-template:0.7
-docker pull cingulara/openrmf-api-compliance:0.7
-docker pull cingulara/openrmf-api-controls:0.7
+docker pull cingulara/openrmf-web:0.8
+docker pull cingulara/openrmf-api-read:0.8
+docker pull cingulara/openrmf-api-save:0.8
+docker pull cingulara/openrmf-api-upload:0.8
+docker pull cingulara/openrmf-api-scoring:0.8
+docker pull cingulara/openrmf-msg-score:0.8
+docker pull cingulara/openrmf-msg-compliance:0.8
+docker pull cingulara/openrmf-msg-controls:0.8
+docker pull cingulara/openrmf-msg-checklist:0.8
+docker pull cingulara/openrmf-templatedb:0.8
+docker pull cingulara/openrmf-scoredb:0.8
+docker pull cingulara/openrmf-checklistdb:0.8
+docker pull cingulara/openrmf-api-template:0.8
+docker pull cingulara/openrmf-api-compliance:0.8
+docker pull cingulara/openrmf-api-controls:0.8
 docker pull nats:1.4.1-linux
 
 REM Now run the latest development openRMF stack
