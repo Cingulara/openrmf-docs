586d867ee67d878ac4744a776a41757438747013
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-09-20 20:29:39 -0400

    fixing the script for linux/mac

--- scripts/keycloak/start.sh
+++ scripts/keycloak/start.sh
@@ -1,8 +1,8 @@
-REM Run the Keycloak Stack
+# Run the Keycloak Stack
 
 docker-compose up -d
 
-REM tell them the URL for Keycloak
-ECHO ""
-ECHO "Run http://{ip-address}:9001/ to access Keycloak locally"
-ECHO ""
+# tell them the URL for Keycloak
+echo ''
+echo 'Run http://{ip-address}:9001/ to access Keycloak locally'
+echo ''
