d1cb1595d45f6a99e8f94ea64041d2d6ef5725c6
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2020-01-05 10:51:28 -0500

    updating indexes

--- scripts/initializeAudit.js
+++ scripts/initializeAudit.js
@@ -1,2 +1,5 @@
 db.createUser({ user: "openrmfaudit" , pwd: "openrmf1234!", roles: [{ "role": "readWrite", "db": "openrmfaudit"}]});
 db.createCollection("Audits");
+db.Audits.createIndex({ created: -1 })
+db.Audits.createIndex({ username: 1 })
+db.Audits.createIndex({ program: 1 })
--- scripts/initializeTemplate.js
+++ scripts/initializeTemplate.js
@@ -1,2 +1,4 @@
 db.createUser({ user: "openrmftemplate" , pwd: "openrmf1234!", roles: [{ "role": "readWrite", "db": "openrmftemplate"}]});
 db.createCollection("Templates");
+db.Templates.createIndex({ stigType: 1 })
+db.Templates.createIndex({ templateType: 1 })
