7f582ad4d225a6829f3e26c8a50c4c450cb5715c
Dale Bingham <dalebingham@MacBook-Pro.localdomain>
2020-04-07 06:39:55 -0400

    #87 adding a few indexes for MongoDB

--- scripts/local/initializedbs.js
+++ scripts/local/initializedbs.js
@@ -7,19 +7,25 @@ db = db.getSiblingDB('openrmf');
 db.createCollection("Artifacts");
 db.Artifacts.createIndex({ systemGroupId: 1 })
 db.Artifacts.createIndex({ stigType: 1 })
+db.Artifacts.createIndex({ stigRelease: 1 })
+db.Artifacts.createIndex({ version: 1 })
 db.createCollection("SystemGroups");
 db.SystemGroups.createIndex({ title: 1 })
 db = db.getSiblingDB('openrmftemplate');
 db.createCollection("Templates");
 db.Templates.createIndex({ stigType: 1 })
 db.Templates.createIndex({ templateType: 1 })
+db.Templates.createIndex({ stigRelease: 1 })
+db.Templates.createIndex({ version: 1 })
 db = db.getSiblingDB('openrmfscore');
 db.createCollection("Scores");
 db.Scores.createIndex({ artifactId: 1 })
 db.Scores.createIndex({ systemGroupId: 1 })
 db.Scores.createIndex({ hostName: 1 })
+db.Scores.createIndex({ stigType: 1 })
 db = db.getSiblingDB('openrmfaudit');
 db.createCollection("Audits");
 db.Audits.createIndex({ created: -1 })
 db.Audits.createIndex({ username: 1 })
 db.Audits.createIndex({ program: 1 })
+db.Audits.createIndex({ action: 1 })
