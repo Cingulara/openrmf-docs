0366a2a509b71b4c5ea32fe1444d9f4396bfa53c
Dale Bingham <dalebingham@Dales-MBP.cingularanet>
2020-07-11 14:03:49 -0400

    Fixing the echo in the SH script

--- scripts/keycloak/start.sh
+++ scripts/keycloak/start.sh
@@ -5,5 +5,5 @@ docker-compose up -d
 # tell them the URL for Keycloak
 echo ''
 echo 'Use http://{ip-address}:9001/ to access Keycloak locally.'
-ECHO 'Run a setup (Linux, Mac, Windows) script to automate the OpenRMF realm creation.'
+echo 'Run a setup (Linux, Mac, Windows) script to automate the OpenRMF realm creation.'
 echo ''
