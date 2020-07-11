7af4431f032127bce99dbdde22a02a529c532cf0
Dale Bingham <dalebingham@Dales-MBP.cingularanet>
2020-07-11 12:41:34 -0400

    #179 Fixing running in Linux - locally and remote

--- scripts/keycloak/start.cmd
+++ scripts/keycloak/start.cmd
@@ -4,5 +4,6 @@ docker-compose up -d
 
 REM tell them the URL for Keycloak
 ECHO ""
-ECHO "Run http://{ip-address}:9001/ to access Keycloak locally"
+ECHO "Use http://{ip-address}:9001/ to access Keycloak locally."
+ECHO "Run a setup (Linux, Mac, Windows) script to automate the OpenRMF realm creation."
 ECHO ""
