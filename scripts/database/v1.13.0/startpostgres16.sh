085adae4f01ee93077c3e6824f6f972eae743731
Dale Bingham <dale.bingham@soteriasoft.com>
2025-03-08 11:15:09 -0500

    updating v1.13 main YAML part 1

---
+++ scripts/database/v1.13.0/startpostgres16.sh
@@ -0,0 +1,5 @@
+# Used to interact, view, troubleshoot data in the MongoDB databases
+docker compose -f ./postgres16upgrade.yml up -d
+echo ''
+echo 'Postgres 16 started to update the OpenRMF OSS Keycloak database.'
+echo ''
---
+++ scripts/database/v1.13.0/stoppostgres16.sh
@@ -0,0 +1,4 @@
+docker compose -f ./postgres16upgrade.yml down
+echo ''
+echo 'The Postgres 16 container has been shut down.'
+echo ''
