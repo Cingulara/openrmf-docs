7a6d23958f7e1478f2ef8ebbeb07bd922215d5cf
Dale Bingham <dale.bingham@soteriasoft.com>
2025-03-08 16:06:37 -0500

    updates for v1.13 upgrade to happen

--- scripts/database/v1.13.0/startpostgres16.sh
+++
@@ -1,5 +0,0 @@
-# Used to interact, view, troubleshoot data in the MongoDB databases
-docker compose -f ./postgres16upgrade.yml up -d
-echo ''
-echo 'Postgres 16 started to update the OpenRMF OSS Keycloak database.'
-echo ''
--- scripts/database/v1.13.0/stoppostgres16.sh
+++
@@ -1,4 +0,0 @@
-docker compose -f ./postgres16upgrade.yml down
-echo ''
-echo 'The Postgres 16 container has been shut down.'
-echo ''
